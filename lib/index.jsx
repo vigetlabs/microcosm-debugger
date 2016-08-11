1878c89ae6ff97cd8ebefeff6775ce47ab57bf68
Lawson Kurtz <lawson.kurtz@viget.com>
2016-08-11 16:10:37 -0600

    Allow specific placement of debugger UI

--- lib/index.jsx
+++ lib/index.jsx
@@ -4,10 +4,10 @@ import Debugger from './components/debugger'
 
 export default function (app, options) {
   var container = document.createElement('div')
-
   container.className = "debugger-wrapper"
 
-  document.body.appendChild(container)
+  var parentContainer = document.getElementById('microcosm-debugger') || document.body
+  parentContainer.appendChild(container)
 
   function checkout (action) {
     app.checkout(action)
